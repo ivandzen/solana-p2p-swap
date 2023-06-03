33925647dc6eaf9a483b258ecad97021827fdce7
Ivan Loboda <loboda.ivan.y@gmail.com>
2023-06-04 00:44:19 +0400

    add debug output

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -182,11 +182,15 @@ impl AppContext {
 
         instruction_data.extend_from_slice(message);
 
-        Instruction {
+        let instr = Instruction {
             program_id: solana_sdk::ed25519_program::id(),
             accounts: vec![],
             data: instruction_data,
-        }
+        };
+
+        println!("{:?}", instr);
+
+        instr
     }
 }
 
@@ -419,6 +423,8 @@ fn process_buy_order(context: &AppContext, args: &Option<&ArgMatches>) {
             accounts,
             data,
         });
+
+        println!("{:?}", instructions);
 
         let signature = context.send_transaction(&instructions).unwrap();
         print!("transaction: {:?}", signature);
