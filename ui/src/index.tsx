7df491286380da4a6f256fcae9d3f749dd5672f1
Ivan Loboda <loboda.ivan.y@gmail.com>
2023-06-14 03:50:52 +0400

    revert part of changes

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,4 +1,3 @@
-/*
 import React from 'react';
 import {createRoot} from 'react-dom/client'
 import { App } from './App';
@@ -6,15 +5,3 @@ import { App } from './App';
 const container = document.getElementById('app');
 const root = createRoot(container!);
 root.render(<App/>);
-*/
-
-import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App';
-
-ReactDOM.render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
-    document.getElementById('app')
-);
